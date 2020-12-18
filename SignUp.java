bdf9c0d1ffaceb89ce649c3f8975371787e44adc
MohamedShaker <mshaker809@yahoo.com>
2020-12-18 15:35:32 +0200

    Creating SignUp class

---
+++ SignUp.java
@@ -0,0 +1,13 @@
+class SignUp{
+	String email;
+	String passwordOne;
+	String PasswordTwo;
+	String userName;
+	
+	SignUp(	String email,String passwordOne,String PasswordTwo,String userName){
+		this. email=email;
+		this.passwordOne=passwordOne;
+		this.PasswordTwo;
+		this.userName=userName;
+	}
+}
