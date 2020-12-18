3db4ab8a4d7092b8ba9ea8259aa7c28de5838eb7
Maha <maha_mekdad@outlook.com>
2020-12-18 15:45:11 +0200

    adding the conflict class for us to write in it

---
+++ ConflictClass.java
@@ -0,0 +1,13 @@
+
+class ConflictClass{
+	
+	public static void conflictMethod(){
+	
+		System.out.println("Maha: modify this print statement");
+	}
+	
+	public static void main(String [] args){
+		
+		conflictMethod();
+	}
+}
