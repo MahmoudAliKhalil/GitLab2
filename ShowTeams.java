c4ae19db3b4f1b2542dfc5553100e0c1bd904a48
Maha <maha_mekdad@outlook.com>
2020-12-18 15:16:50 +0200

    adding the showTeams feature

---
+++ ShowTeams.java
@@ -0,0 +1,15 @@
+
+class ShowTeams{
+	
+	public static void showTeams(){
+	
+		System.out.println("Team1!");
+		System.out.println("Team2!!");
+		System.out.println("Team3!!!");
+	}
+	
+	public static void main(String [] args){
+		
+		showTeams();
+	}
+}
