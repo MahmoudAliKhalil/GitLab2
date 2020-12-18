abe708421b8a8b1e6755d429f63f226cf8758abc
MAK <mahmoud.alikhalil@gmail.com>
2020-12-18 15:51:05 +0200

    Changing the print messege

--- ConflictClass.java
+++ ConflictClass.java
@@ -3,7 +3,7 @@ class ConflictClass{
 	
 	public static void conflictMethod(){
 	
-		System.out.println("Maha: modify this print statement");
+		System.out.println("Mahmoud: modify this print statement");
 	}
 	
 	public static void main(String [] args){
