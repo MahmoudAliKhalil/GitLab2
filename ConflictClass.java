d8670498e4f7224477d9312965535403f6eeebf0
MohamedShaker <mshaker809@yahoo.com>
2020-12-18 16:31:12 +0200

    ll

--- ConflictClass.java
+++ ConflictClass.java
@@ -2,8 +2,10 @@
 class ConflictClass{
 	
 	public static void conflictMethod(){
+
 	
 		System.out.println("Shaker : modify this print statement");
+
 	}
 	
 	public static void main(String [] args){
